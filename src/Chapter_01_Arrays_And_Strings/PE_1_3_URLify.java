70714f0da118ce885e484d781809c2f196e613a0
Kevin Guan <Kevin Guan>
2017-02-09 14:40:28 -0500

    CHP01 PE03 Incomplete

---
+++ src/Chapter_01_Arrays_And_Strings/PE_1_3_URLify.java
@@ -0,0 +1,28 @@
+package Chapter_01_Arrays_And_Strings;
+
+import java.util.Scanner;
+
+/**
+ * URLify
+ * Write a method to replace all spaces in a string with '%20'. 
+ * You may assume that the string has sufficient space at the end to hold the additional characters, 
+ * and that you are given the "true" length of the string. 
+ * (Note: If implementing in Java, please use a character array so that you can perform this operation in place.)
+ * 
+ * 02/
+ * @author kevgu
+ *
+ */
+
+public class PE_1_3_URLify 
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Enter a string: ");
+		Scanner input = new Scanner(System.in);
+		
+		
+		input.close();
+	}
+	
+}
